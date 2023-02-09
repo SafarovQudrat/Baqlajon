0821c4e6f686e0163792a206d3a808a39d6062e3
Xusanov00 <93189344+Xusanov00@users.noreply.github.com>
2023-02-09 17:36:11 +0500

    bugs with BNColor age fixed

---
+++ Baqlajon/Controller/OtpVC.swift
@@ -0,0 +1,23 @@
+//
+//  OtpVC.swift
+//  Baqlajon
+//
+//  Created by Ali on 08/02/23.
+//
+
+import UIKit
+
+class OtpVC: UIViewController {
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+    
+    
+    
+}
