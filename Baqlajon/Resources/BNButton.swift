def4f661eb12175a95ba8a6efbdb27e30a07497d
Chingiz Jumanov <normuhammadovich@gmail.com>
2023-02-09 14:05:14 +0500

    custom btn and custom txtfield is done

--- Baqlajon/Resources/BNButton.swift
+++ Baqlajon/Resources/BNButton.swift
@@ -11,14 +11,25 @@ class BNButton: UIButton {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        configure()
     }
     
     required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        self.backgroundColor = .appColor(color: .mainBlue)
-        self.titleLabel?.font = .appFont(ofSize: 16, weight: .medium)
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor = .appColor(color: .mainBlue), title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        
+    }
+    
+    func configure() {
         self.layer.cornerRadius = 8
-        self.setTitleColor(.white, for: .normal)
+        self.titleLabel?.font = .appFont(ofSize: 16, weight: .medium)
+        setTitleColor(.white, for: .normal)
+        translatesAutoresizingMaskIntoConstraints = false
     }
     
 }
